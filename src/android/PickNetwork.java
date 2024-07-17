bbb0970338fe1202fc9b889367f2fdc895b25c20
Kevin Patrick Tan <kevin@orangekloud.com>
2024-07-17 14:07:39 +0800

    EP-1188 Added same baselines.

--- src/android/PickNetwork.java
+++ src/android/PickNetwork.java
@@ -1,26 +1,73 @@
 package com.orangekloud.plugins.pick_network;
 
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.cainiao.cntech.blebox.CNTechLeHelper;
+
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 
 public class PickNetwork extends CordovaPlugin {
 
+    final String[] BLE_PERMISSIONS = new String[]{
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+    };
+
     /**
      * Executes the request.
      */
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
-        // Send Payment Request
-        if (action.equals("sendPaymentRequest")) { 
-            // args.getString(0);
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+//        if (action.equals("..")) {
+//            checkSelfPermissions();
+            // Initialize
+            CNTechLeHelper.initLe(this.cordova.getActivity(), false);
+
+            // Start Scan
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (!bluetoothAdapter.isEnabled()) {
+                // Bluetooth is disabled
+                Toast.makeText(this.cordova.getActivity(), "Disabled bluetooth", Toast.LENGTH_LONG).show();
+                return true;
+            }
+
+            // Stop Scan
+//            CNTechLeHelper.cancelScan();
+
+            // Destroy
+            CNTechLeHelper.destroy();
             return true;
+//        }
+//        return false;
+    }
+
+    /**
+     * Check if permission is already enabled,
+     * if not initiate a request to the user.
+     */
+    private boolean checkSelfPermissions() {
+        boolean result = true;
+        try {
+            Activity activity = this.cordova.getActivity();
+            if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                    ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(activity, BLE_PERMISSIONS, 0);
+                result = false;
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            result = false;
         }
-
-        // Context cordovaContext = this.cordova.getActivity().getApplicationContext();
-
-        // callbackContext.error("Missing any of the following fields: Merchant ID and Payment Token");
-        return false;
+        return result;
     }
 
 }
--- www/PickNetwork.js
+++ www/PickNetwork.js
@@ -3,5 +3,29 @@
  */
 let pickNetworkExport = {};
 
+/**
+ * Payment Request
+ */ 
+// ccppExport.sendPaymentRequest = function (successCallback, errorCallback, options) {
+//     options = options || {};
+
+//     var args = [];
+
+//     args.push(getValue(options.environment, "sandbox"));
+//     args.push(getValue(options.merchantID, null));
+//     args.push(getValue(options.paymentToken, null));
+
+//     args.push(getValue(options.paymentType, "cc"));
+//     args.push(getValue(options.agentCode, null));
+//     args.push(getValue(options.agentChannelCode, null));
+
+//     args.push(getValue(options.creditCardNo, null));
+//     args.push(getValue(options.creditCardExpiryMonth, 0));
+//     args.push(getValue(options.creditCardExpiryYear, 0));
+//     args.push(getValue(options.creditCardSecurityCode, null));
+    
+//     exec(successCallback, errorCallback, 'CCPP', 'sendPaymentRequest', args);
+// };
+
 // Export the functions
 module.exports = pickNetworkExport;
